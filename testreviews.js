f3865ba9b092304b9a8d0ce10be1b33d8a1e71b2
Emuolanari <emuolanari@gmail.com>
2015-03-02 12:53:01 +0000

    add testreviews file

--- testreviews.js
+++ testreviews.js
@@ -1,5 +1,3 @@
-// HELLO!!
-
 var restify = require('restify');
 var request = require('request');
 var rand = require('csprng');
@@ -12,11 +10,14 @@ server.use(restify.authorizationParser());
 
 server.listen(8080, function() {
 	console.log('incoming request being handled');
-	server.put(/^\/books\/([a-z]+)$/, function(req, res, next) {
+	server.get(/^\/reviews.(json|xml)/, function(req, res, next) {
+        console.log('/reviews.json');
+        res.end();
+    });
+	server.put(/^\/reviews\/([a-z]+)$/, function(req, res, next) {
 		console.log('PUT '+req.params[0]);
-		if (req.params[0] != req.authorization.basic.username) {
-			return next(new restify.ForbiddenError('mismatched username and url'));
-		}
+		console.log('user: '+JSON.stringify(req.authorization));
+		console.log(req.params[0]);
 		if (req.headers['content-type'] != 'application/json') {
 			return next(new restify.UnsupportedMediaTypeError('bad Content-Type'));
 		}
@@ -24,8 +25,8 @@ server.listen(8080, function() {
 			return next(new restify.UnauthorizedError('Basic HTTP auth required'));
 		}
 		console.log('parameters supplied');
-		var url = 'http://liberal-button.codio.io:5984/books/'+req.params[0];
-		var items = req.params['books'];
+        var url = 'http://congo-bagel.codio.io:5984/reviews/'+req.params[0];
+		var review = req.params['review'];
 		request.get(url, function(err, response, body) {
 			if (response.statusCode == 404) {
 				var salt = rand(160, 36);
@@ -39,7 +40,9 @@ server.listen(8080, function() {
 					last_modified: date,
 					password: password,
 					salt: salt,
-					items: items
+                    username: review['username'],
+					book: review['book'],
+					content: review['content']
 				};
 				var docStr = JSON.stringify(doc);
 				var params = {uri:url, body: JSON.stringify(doc)};
@@ -53,13 +56,37 @@ server.listen(8080, function() {
 					res.setHeader('Last-Modified', date);
 					res.setHeader('Content-Type', 'application/json');
 					res.setHeader('Accepts', 'PUT');
-					res.send({books: req.params['books']});
+					res.send({reviews: req.params['reviews']});
 					res.end();
 				});
 				console.log('document not found');
 			}
 			if (response.statusCode == 200) {
 				console.log('existing document');
+                body = JSON.parse(body);
+                var pwd = sha1(req.authorization.basic.password+body.salt);
+                if (pwd != body.password) {
+                  return next(new restify.ForbiddenError('invalid username/password'));
+                }	
+                console.log('passwords match!');
+                var d = new Date();
+                var date = d.toUTCString()
+                body.last_modified = date;
+                body.reviews = reviews;
+                console.log(body);
+                var params = {uri:url, body: JSON.stringify(doc)};
+                  request.put(params, function(err, response, body) {
+                    if (err) {
+                      return next(new restify.InternalServerError('Cannot create document'));
+                    }
+                    // document has been inserted into database
+                    res.setHeader('Location', 'http://'+req.headers.host+req.url);
+					res.setHeader('ETag', body.rev);
+					res.setHeader('Last-Modified', date);
+					res.setHeader('Content-Type', 'application/json');
+                    res.send({reviews: req.params['reviews']});
+                    res.end();
+                });
 			}
 		});
 		res.end();
